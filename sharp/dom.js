539f471bae74541646b2b985adab49629c13376a
ashwinc17 <ashwinc00017@gmail.com>
2023-11-24 20:12:06 +0530

    completed task

--- sharp/dom.js
+++ sharp/dom.js
@@ -1,3 +1,4 @@
+// DOM \\
 //console.dir(document);
 // console.log(document.domain);
 // console.log(document.URL);
@@ -24,18 +25,48 @@
 //items[1].style.fontWeight = 'bold';
 //items[1].style.backgroundColor = 'yellow';
 //items[2].style.backgroundColor = 'green';
-
 // for(let i=0; i<items.length; i++) {
 //     items[i].style.fontWeight = 'bold'
 // }
 
 //get element by tag name
-let li = document.getElementsByTagName('li');
-li[1].textContent = 'hey'
-
+// let li = document.getElementsByTagName('li');
+// li[1].textContent = 'hey'
 // for(let i=0; i<li.length; i++) {
 //     li[i].style.backgroundColor = 'green';
 // }
-let newItem = document.createElement('li');
-newItem.textContent = "item 5"
-li[3].appendChild(newItem);
+// let newItem = document.createElement('li');
+// newItem.textContent = "item 5"
+// li[3].appendChild(newItem);
+
+// Query Selector
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc'
+// let input = document.querySelector('input');
+// input.value = 'new'
+// let submit = document.querySelector('input[type = "submit"]');
+// submit.value = "send"
+// let item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// let lastitem = document.querySelector('.list-group-item:last-child');
+// lastitem.style.color = 'orange';
+
+// Query Selector ALL
+// let titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'hello';
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// for(let i=0; i<odd.length; i++) {
+//     odd[i].style.backgroundColor = 'grey';
+// }
+
+//Task
+
+let li = document.querySelectorAll('li');
+li[1].style.color = 'green';
+
+let titles = document.querySelectorAll('.title');
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0; i<odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
+}
