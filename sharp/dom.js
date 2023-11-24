39301444676c4e723d937db19d11897b8f55f161
ashwinc17 <ashwinc00017@gmail.com>
2023-11-24 19:54:39 +0530

    added new item

--- sharp/dom.js
+++ sharp/dom.js
@@ -17,14 +17,25 @@
 // header.style.borderBottom = 'solid 10px #002'
 
 // Get elements by class name 
-let items = document.getElementsByClassName('list-group-item');
+//let items = document.getElementsByClassName('list-group-item');
 //console.log(items);
 //console.log(items[1]);
-items[1].textContent = 'hello 2';
+//items[1].textContent = 'hello 2';
 //items[1].style.fontWeight = 'bold';
 //items[1].style.backgroundColor = 'yellow';
-items[2].style.backgroundColor = 'green';
+//items[2].style.backgroundColor = 'green';
 
-for(let i=0; i<items.length; i++) {
-    items[i].style.fontWeight = 'bold'
-}
+// for(let i=0; i<items.length; i++) {
+//     items[i].style.fontWeight = 'bold'
+// }
+
+//get element by tag name
+let li = document.getElementsByTagName('li');
+li[1].textContent = 'hey'
+
+// for(let i=0; i<li.length; i++) {
+//     li[i].style.backgroundColor = 'green';
+// }
+let newItem = document.createElement('li');
+newItem.textContent = "item 5"
+li[3].appendChild(newItem);
