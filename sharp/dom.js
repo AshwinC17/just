67bfaa333c5bd1ec3f28d63cbf92b985c4b0e437
ashwinc17 <ashwinc00017@gmail.com>
2023-11-25 19:05:56 +0530

    Creating Nodes and Modifying Dom

--- sharp/dom.js
+++ sharp/dom.js
@@ -6,7 +6,7 @@
 // console.log(document.links);
 
 
-// Get elements by id
+// Get element by id
 //console.log(document.getElementById('header-title'));
 // let headerTitle = document.getElementById('header-title');
 // let header = document.getElementById('main-header');
@@ -29,7 +29,7 @@
 //     items[i].style.fontWeight = 'bold'
 // }
 
-//get element by tag name
+//Get element by tag name
 // let li = document.getElementsByTagName('li');
 // li[1].textContent = 'hey'
 // for(let i=0; i<li.length; i++) {
@@ -61,12 +61,95 @@
 // }
 
 //Task
+// let li = document.querySelectorAll('li');
+// li[1].style.color = 'green';
+// let titles = document.querySelectorAll('.title');
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// for(let i=0; i<odd.length; i++) {
+//     odd[i].style.backgroundColor = 'green';
+// }
 
-let li = document.querySelectorAll('li');
-li[1].style.color = 'green';
+// Traverse \\
 
-let titles = document.querySelectorAll('.title');
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0; i<odd.length; i++) {
-    odd[i].style.backgroundColor = 'green';
-}
+//let il = document.querySelector('#items');
+
+//ParentNode 
+// console.log(il.parentNode);
+// console.log(il.parentNode);
+// il.parentNode.style.background = 'grey';
+// console.log(il.parentNode.parentNode.parentNode);
+
+//Parent Element
+// console.log(il.parentElement);
+// console.log(il.parentElement);
+// il.parentNode.style.background = 'grey';
+// console.log(il.parentElement);
+
+// ChildNodes
+// console.log(il.childNodes);
+// console.log(il.children);
+// console.log(il.children[1]);
+// il.children[1].style.backgroundColor = 'yellow';
+
+//FirstChild
+// console.log(il.firstChild);
+
+// First element child
+// console.log(il.firstElementChild);
+// il.firstElementChild.textContent = " hello first";
+
+//Last element Child
+//console.log(il.lastElementChild);
+// il.lastElementChild.textContent = " hello last";
+
+//Last Child
+// console.log(il.lastChild);
+// il.lastChild.textContent = " hello last";
+
+//nextsibling
+// console.log(il.nextSibling);
+
+//next element sibling
+// console.log(il.nextElementSibling);
+
+//previous sibling
+// console.log(il.previousSibling);
+
+//previous  element sibling
+// console.log(il.previousElementSibling);
+// il.previousElementSibling.style.backgroundColor = 'yellow';
+
+// create Element
+let newDiv = document.createElement('div');
+
+// add class
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'h1';
+
+// set attribute
+newDiv.setAttribute('title', 'hello div');
+
+// create text node
+let newDivTxt = document.createTextNode('Hello World');
+
+// add text to div/ append child
+newDiv.appendChild(newDivTxt);
+
+// Insert before "Item Lister"
+let container1 = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+container1.insertBefore(newDiv, h1);
+newDiv.style.fontSize = '20px';
+
+// Update id and title for the next use
+newDiv.id = 'h2';
+newDiv.setAttribute('title', 'hello2 div');
+
+// Insert before "Item 1"
+let container2 = document.querySelector('#items');
+let item1 = document.querySelector('#items li:first-child');
+container2.insertBefore(newDiv.cloneNode(true), item1);
+
+newDiv.style.fontSize = '50px';
