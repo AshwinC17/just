6a4e69b72badd6e48312b92882157a6c3f293061
ashwinc17 <ashwinc00017@gmail.com>
2023-11-26 17:50:49 +0530

    added delete option

--- sharp1/main.js
+++ sharp1/main.js
@@ -15,6 +15,33 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     existingUsers.push(userDetails);
     localStorage.setItem('users', JSON.stringify(existingUsers));
   
+    displayUserDetails(userDetails);
+  
     alert('User details saved to local storage!');
   });
+  
+  function displayUserDetails(userDetails) {
+    let userDiv = document.createElement('div');
+    userDiv.innerHTML = `<p>Name: ${userDetails.name}</p><p>Email: ${userDetails.email}</p><p>Phone: ${userDetails.phone}</p>`;
+  
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function() {
+      removeUserDetails(userDetails);
+      userDiv.remove();
+    });
+  
+    userDiv.appendChild(deleteButton);
+  
+    document.body.appendChild(userDiv);
+  }
+  
+  function removeUserDetails(userDetailsToRemove) {
+    let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+    let updatedUsers = existingUsers.filter(user => {
+      return user.email !== userDetailsToRemove.email;
+    });
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  }
+  
   
