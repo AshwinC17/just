4ba28d94985a4ac95abc1c74d6fa5df8fceac30d
ashwinc17 <ashwinc00017@gmail.com>
2023-12-25 11:58:15 +0530

    added  dom loaded

--- sharp1/main.js
+++ sharp1/main.js
@@ -1,70 +1,86 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Make a GET request to crudcrud.com using Axios to retrieve saved user details
+  axios.get('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new')
+      .then(function(response) {
+          // Handle the success response here
+          console.log('User details retrieved from cloud:', response.data);
+
+          // Display the retrieved user details on the website
+          response.data.forEach(function(userDetails) {
+              displayUserDetails(userDetails);
+          });
+      })
+      .catch(function(error) {
+          // Handle errors here
+          console.error('Error retrieving user details:', error);
+      });
+});
+
 document.getElementById('userForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
 
-    let userDetails = {
-        name: name,
-        email: email,
-        phone: phone
-    };
+  let userDetails = {
+      name: name,
+      email: email,
+      phone: phone
+  };
 
-    // POST request to crudcrud.com using Axios
-    axios.post('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new', userDetails)
-        .then(function(response) {
-            // Handle the success response here if needed
-            console.log('User details saved to cloud:', response.data);
+  // Make a POST request to crudcrud.com using Axios to save user details
+  axios.post('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new', userDetails)
+      .then(function(response) {
+          // Handle the success response here if needed
+          console.log('User details saved to cloud:', response.data);
 
-            // Call the displayUserDetails function with the response data
-            displayUserDetails(response.data);
+          // Call the displayUserDetails function with the response data
+          displayUserDetails(response.data);
 
-            // clear the form after successful submission
-            clearForm();
-        })
-        .catch(function(error) {
-            // Handle errors here
-            console.error('Error saving user details:', error);
-        });
+          //clear the form after successful submission
+          clearForm();
+      })
+      .catch(function(error) {
+          // Handle errors here
+          console.error('Error saving user details:', error);
+      });
 });
 
 function displayUserDetails(userDetails) {
-    let userDiv = document.createElement('div');
-    userDiv.innerHTML = `<p>Name: ${userDetails.name}</p><p>Email: ${userDetails.email}</p><p>Phone: ${userDetails.phone}</p>`;
+  let userDiv = document.createElement('div');
+  userDiv.innerHTML = `<p>Name: ${userDetails.name}</p><p>Email: ${userDetails.email}</p><p>Phone: ${userDetails.phone}</p>`;
 
-    let editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    editButton.addEventListener('click', function() {
-        editUserDetails(userDetails);
-        userDiv.remove(); // Remove the displayed details after editing
-    });
+  let editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.addEventListener('click', function() {
+      editUserDetails(userDetails);
+      userDiv.remove(); // Remove the displayed details after editing
+  });
 
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function() {
-        removeUserDetails(userDetails);
-        userDiv.remove();
-    });
+  let deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', function() {
+      removeUserDetails(userDetails);
+      userDiv.remove();
+  });
 
-    userDiv.appendChild(editButton);
-    userDiv.appendChild(deleteButton);
+  userDiv.appendChild(editButton);
+  userDiv.appendChild(deleteButton);
 
-    document.body.appendChild(userDiv);
+  document.body.appendChild(userDiv);
 }
 
 function editUserDetails(userDetailsToEdit) {
-    // editing user details in your cloud storage
-    axios.put('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToEdit._id, updatedUserDetails)
+  axios.put('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToEdit._id, updatedUserDetails)
 }
 
 function removeUserDetails(userDetailsToRemove) {
-    // deleting user details in your cloud storage
-    axios.delete('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToRemove._id)
+  axios.delete('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToRemove._id)
 }
 
 function clearForm() {
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('phone').value = '';
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('phone').value = '';
 }
