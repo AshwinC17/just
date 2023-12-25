0b6eef15b25bdc4ba7b31045a871e02150a1ed5d
ashwinc17 <ashwinc00017@gmail.com>
2023-12-25 11:54:10 +0530

    added cloud storage feature

--- sharp1/main.js
+++ sharp1/main.js
@@ -1,71 +1,70 @@
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault();
-  
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let phone = document.getElementById('phone').value;
-  
+
     let userDetails = {
-      name: name,
-      email: email,
-      phone: phone
+        name: name,
+        email: email,
+        phone: phone
     };
-  
-    let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    existingUsers.push(userDetails);
-    localStorage.setItem('users', JSON.stringify(existingUsers));
-  
-    displayUserDetails(userDetails);
-  
-    alert('User details saved to local storage!');
-    clearForm();
-  });
-  
-  function displayUserDetails(userDetails) {
+
+    // POST request to crudcrud.com using Axios
+    axios.post('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new', userDetails)
+        .then(function(response) {
+            // Handle the success response here if needed
+            console.log('User details saved to cloud:', response.data);
+
+            // Call the displayUserDetails function with the response data
+            displayUserDetails(response.data);
+
+            // clear the form after successful submission
+            clearForm();
+        })
+        .catch(function(error) {
+            // Handle errors here
+            console.error('Error saving user details:', error);
+        });
+});
+
+function displayUserDetails(userDetails) {
     let userDiv = document.createElement('div');
     userDiv.innerHTML = `<p>Name: ${userDetails.name}</p><p>Email: ${userDetails.email}</p><p>Phone: ${userDetails.phone}</p>`;
-  
+
     let editButton = document.createElement('button');
     editButton.textContent = 'Edit';
     editButton.addEventListener('click', function() {
-      editUserDetails(userDetails);
-      userDiv.remove(); // Remove the displayed details after editing
+        editUserDetails(userDetails);
+        userDiv.remove(); // Remove the displayed details after editing
     });
-  
+
     let deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', function() {
-      removeUserDetails(userDetails);
-      userDiv.remove();
+        removeUserDetails(userDetails);
+        userDiv.remove();
     });
-  
+
     userDiv.appendChild(editButton);
     userDiv.appendChild(deleteButton);
-  
+
     document.body.appendChild(userDiv);
-  }
-  
-  function editUserDetails(userDetailsToEdit) {
-    // Remove the existing user details from local storage
-    removeUserDetails(userDetailsToEdit);
-  
-    // Populate the form fields with the existing details for modification
-    document.getElementById('name').value = userDetailsToEdit.name;
-    document.getElementById('email').value = userDetailsToEdit.email;
-    document.getElementById('phone').value = userDetailsToEdit.phone;
-  }
-  
-  function removeUserDetails(userDetailsToRemove) {
-    let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    let updatedUsers = existingUsers.filter(user => {
-      return user.email !== userDetailsToRemove.email;
-    });
-    localStorage.setItem('users', JSON.stringify(updatedUsers));
-  }
-  
-  function clearForm() {
+}
+
+function editUserDetails(userDetailsToEdit) {
+    // editing user details in your cloud storage
+    axios.put('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToEdit._id, updatedUserDetails)
+}
+
+function removeUserDetails(userDetailsToRemove) {
+    // deleting user details in your cloud storage
+    axios.delete('https://crudcrud.com/api/f2ef75475adb446c87a2b76ea66f11e4/new/' + userDetailsToRemove._id)
+}
+
+function clearForm() {
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
     document.getElementById('phone').value = '';
-  }
-  
+}
