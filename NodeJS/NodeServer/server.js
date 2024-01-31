7eb3eb88f8d6d30fadf9e6f307164c291a2e3cd3
ashwinc17 <ashwinc00017@gmail.com>
2024-01-31 17:06:24 +0530

    Based on the url the user hits, it will return custom responses

--- NodeJS/NodeServer/server.js
+++ NodeJS/NodeServer/server.js
@@ -1,8 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Ashwin C');
+  const url = req.url;
+  if (url === '/home') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body><h1>Welcome home</h1></body>');
+    res.write('</html>');
+    res.end();
+  } else if (url === '/about') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    res.end();
+  } else if (url === '/node') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
+  }
 });
 
-
-server.listen(4000);
+server.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000/');
+});
