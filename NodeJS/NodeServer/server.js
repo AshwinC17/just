f521b48433c50c8ae9f29aa7b04e49c904edd4cb
ashwinc17 <ashwinc00017@gmail.com>
2024-01-31 18:00:19 +0530

    parsing request bodies

--- NodeJS/NodeServer/server.js
+++ NodeJS/NodeServer/server.js
@@ -42,12 +42,21 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method == 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
-  
+
   res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body><h1>Welcome to my Node Js project</h1></body>');
