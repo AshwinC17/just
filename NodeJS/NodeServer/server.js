a1a2ade3393d3767a3de444393e7a78dd8aae997
ashwinc17 <ashwinc00017@gmail.com>
2024-02-01 17:15:10 +0530

    Exporting Modules in Different Ways

--- NodeJS/NodeServer/server.js
+++ NodeJS/NodeServer/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 // const server = http.createServer((req, res) => {
 //   const url = req.url;
@@ -28,54 +28,7 @@ const fs = require('fs');
 //   console.log('Server is running on http://localhost:4000/');
 // });
 
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if(url === '/') {
-    const existingMessages = fs.readFileSync('message.txt', 'utf8').split('\n').reverse();
-
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body>');
-
-    existingMessages.forEach((message) => {
-      if (message.trim() !== '') {
-        res.write(`<div>${message}</div>`);
-      }
-    });
-
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (url === '/message' && method == 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      
-      fs.appendFileSync('message.txt', message + '\n', (err) => {
-        if (err) console.error(err);
-      });
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      res.end();
-    });
-    return;
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-    res.write('</html>');
-    res.end();
-})
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
