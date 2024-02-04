0be228b1fffefd077773027e76b0568672e9ca0c
ashwinc17 <ashwinc00017@gmail.com>
2024-02-04 18:32:36 +0530

    Enhanced Product Form with Size Input

--- NodeJS/ExpressJS/app.js
+++ NodeJS/ExpressJS/app.js
@@ -5,9 +5,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<html><form action="/product" method="POST"> <input type = "text" name="title"> <button type = "submit">Add Product</button></form></html>');
-});
+app.use('/add-product', (req, res) => {
+    res.send(`
+      <form action="/add-product" method="POST">
+        <input type="text" name="productName" placeholder="Product Name">
+        <input type="text" name="productSize" placeholder="Product Size">
+        <button type="submit">Add Product</button>
+      </form>
+    `);
+  });
 
 app.post('/product', (req, res, next) => {
     console.log(req.body);
