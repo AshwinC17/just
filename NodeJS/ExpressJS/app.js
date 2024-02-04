5bf96d0073ea9e07d818f49ae6a0da0f7622cb0f
ashwinc17 <ashwinc00017@gmail.com>
2024-02-04 18:35:36 +0530

    Fixed Form Action for Proper Data Handling

--- NodeJS/ExpressJS/app.js
+++ NodeJS/ExpressJS/app.js
@@ -7,13 +7,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res) => {
     res.send(`
-      <form action="/add-product" method="POST">
+      <form action="/product" method="POST">
         <input type="text" name="productName" placeholder="Product Name">
         <input type="text" name="productSize" placeholder="Product Size">
         <button type="submit">Add Product</button>
       </form>
     `);
-  });
+});
 
 app.post('/product', (req, res, next) => {
     console.log(req.body);
