f09a19a43940eca031fcaec1d84c2b0b1b2072d2
ashwinc17 <ashwinc00017@gmail.com>
2024-02-06 16:57:16 +0530

    Returning a 404 Page

--- NodeJS/ExpressJS/app.js
+++ NodeJS/ExpressJS/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,11 +11,10 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
-    next();
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
