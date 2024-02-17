16b1ac26de2d63f55d71e75751158103042ee18f
ashwinc17 <ashwinc00017@gmail.com>
2024-02-17 18:48:16 +0530

    Added Delete Option, Viewing Booked Appointments and Improved Styles

--- Appointments/controllers/appointmentController.js
+++ Appointments/controllers/appointmentController.js
@@ -5,21 +5,17 @@ exports.createAppointment = async (req, res) => {
   try {
     const { name, email, phone } = req.body;
     const appointment = await Appointment.create({ name, email, phone });
-    res.redirect('/success.html'); // Redirect to success page
+    res.redirect('/success.html');
   } catch (error) {
     res.status(500).json({ message: 'Appointment creation failed', error: error.message });
   }
 };
 
-
-// Controller to fetch all appointments
 exports.getAppointments = async (req, res) => {
   try {
     const appointments = await Appointment.findAll();
-    res.status(200).json({ appointments });
+    res.status(200).render('booked-appointments', { appointments });
   } catch (error) {
     res.status(500).json({ message: 'Failed to fetch appointments', error: error.message });
   }
 };
-
-
--- Appointments/routes/appointmentRoutes.js
+++ Appointments/routes/appointmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Appointment = require('../models/appointment');
 const appointmentController = require('../controllers/appointmentController');
 
 // Define a route for booking an appointment
@@ -17,6 +18,25 @@ router.get('/success', (req, res) => {
 });
 
 // Define a route for viewing booked appointments
-router.get('/booked-appointments', appointmentController.getAppointments);
+// Define a route for viewing booked appointments
+router.get('/api/appointments', async (req, res) => {
+  try {
+    const appointments = await Appointment.findAll();
+    res.status(200).json({ appointments });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch appointments', error: error.message });
+  }
+});
+
+router.delete('/api/appointments/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Appointment.destroy({ where: { id } });
+    res.sendStatus(204); // No content, successful delete
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete appointment', error: error.message });
+  }
+});
+
 
 module.exports = router;
