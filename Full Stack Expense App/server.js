66e6c7e5031a5549ca959f888fd8688911c3ee9a
ashwinc17 <ashwinc00017@gmail.com>
2024-02-18 17:49:37 +0530

    Synced Database

--- Full Stack Expense App/server.js
+++ Full Stack Expense App/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const sequelize = require('./util/database');
+const Expense = require('./models/expense');
 
 const app = express();
+const port = 3000;
 
-app.listen(3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database synced');
+  })
+  .catch(err => {
+    console.error('Error syncing database:', err);
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
