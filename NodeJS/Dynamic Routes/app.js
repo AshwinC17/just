a35afb0bc23ce1113f5e964c1f9e2f129c4709ff
ashwinc17 <ashwinc00017@gmail.com>
2024-02-19 20:10:37 +0530

    Creating and Fetching a Cart

--- NodeJS/Dynamic Routes/app.js
+++ NodeJS/Dynamic Routes/app.js
@@ -57,6 +57,10 @@ sequelize
     })
     .then(user => {
         //console.log(user);
+        return user.createCart();
+
+    })
+    .then(user => {
         app.listen(3000);
     })
     .catch(err => {
