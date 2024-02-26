d97eb08e65b4f9ef9c92e92e377faa129bc82871
ashwinc17 <ashwinc00017@gmail.com>
2024-02-26 18:43:25 +0530

    deleted

--- Database Management/util/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('database-management', 'root', 'password', {
-    dialect: 'mysql',
-    host: 'localhost',
-    logging: console.log
-});
-
-module.exports = sequelize;
